d81583a9773147adb3ef22bf6bcd44ae346eb198
rikellyh <rikellyh898@gmail.com>
2025-02-13 00:59:43 -0300

    feat: preparing the development environment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  i18n: {
+    locales: ["pt-BR", "en-US"],
+    defaultLocale: "pt-BR",
+  },
 };
 
 export default nextConfig;
