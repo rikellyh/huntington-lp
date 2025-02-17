5fc2ac98e2845d3b8c5937d1d0e05c3a2e612c68
Rikelly Mendes <102254879+rikellyh@users.noreply.github.com>
2025-02-17 16:36:41 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  i18n: {
-    locales: ["pt-BR", "en-US"],
-    defaultLocale: "pt-BR",
-  },
 };
 
 export default nextConfig;
