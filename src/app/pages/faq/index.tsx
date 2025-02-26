b1c3175c8170002bd72ebcde4461ce037454d007
rikellyh <rikellyh898@gmail.com>
2025-02-26 00:42:20 -0300

    feat(Faq): add arrow icon and fix css

--- src/app/pages/faq/index.tsx
+++ src/app/pages/faq/index.tsx
@@ -1,11 +1,13 @@
 "use client";
 import { useState } from "react";
 
+import Image from "next/image";
 import { accordionContentData } from "@/app/data/faq/accordionContent";
 import { FieldTypeInput } from "@/app/components/layout/sections/faq/fieldTypeInput";
 import Accordion from "@/app/components/layout/sections/faq";
 
 import { rubik } from "../../../../public/assets/fonts/fonts";
+import ArrowRight from "../../../../public/assets/icons/arrow-right.svg";
 
 export default function Faq() {
   const [openIndex, setOpenIndex] = useState<number | null>(null);
@@ -19,11 +21,11 @@ export default function Faq() {
       id="faq"
       className={`${rubik.className} bg-support-600 text-support-50 flex flex-col items-center`}
     >
-      <div className="py-10 text-center max-w-4xl w-full">
+      <div className="py-10 text-center w-full">
         <div className="text-3xl mb-12">
           <h1>PERGUNTAS FREQUENTES</h1>
         </div>
-        <div className="max-w-[52rem] w-full">
+        <div className="max-w-[52rem] w-full mx-auto">
           {accordionContentData.map((item, index) => (
             <Accordion
               key={index}
@@ -43,9 +45,12 @@ export default function Faq() {
               <p className="mb-4">
                 Preencha seus dados e receba mais informações sobre o evento.
               </p>
-              <span className="font-light">
-                Cadastre-se na nossa newsletter ao lado
-              </span>
+              <div className="flex items-center gap-2">
+                <span className="font-light">
+                  Cadastre-se na nossa newsletter ao lado
+                </span>
+                <Image src={ArrowRight} alt="arrow right " />
+              </div>
             </div>
           </div>
           <div className="flex flex-col gap-2 w-1/2 max-w-[28.25rem]">
@@ -69,8 +74,8 @@ export default function Faq() {
           </div>
         </div>
       </div>
-      <div className="py-20 text-center max-w-4xl w-full">
-        <div className="flex justify-between items-center mx-auto max-w-7xl">
+      <div className="py-20 text-center w-full">
+        <div className="flex justify-center items-center mx-auto max-w-7xl">
           <div className="w-1/2 max-w-[32.5rem] flex flex-col items-start gap-8 text-start">
             <h1 className="text-3xl">COMO CHEGAR</h1>
             <div>
